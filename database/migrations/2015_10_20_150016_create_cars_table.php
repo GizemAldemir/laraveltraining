8210e8c07e3bb83a0a75726f49797d7a34cc4b4a
GizemAldemir <aldemirgizem@gmail.com>
2015-10-20 18:11:38 +0300

    update

---
+++ database/migrations/2015_10_20_150016_create_cars_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('brand');
+            $table->text('properties');
+            $table->timestamps();
+            $table->timestamp('sale_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cars');
+    }
+}
